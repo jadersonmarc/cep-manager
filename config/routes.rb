d823d3ad640823d0a0329a80d79c6a1b30c7b76b
jadersonmarc <jadersonmarc@gmail.com>
2021-03-20 21:24:43 -0300

    create controller users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   namespace :api, defaults: { format: :json } do
-    resources :users, only: %i[index]
+    resources :users, only: %i[show create update destroy]
     resources :addresses, only: %i[index]
   end
- 
 end
