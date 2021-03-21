d2cf841069b2a813cd11558e18cd668a1a2948bb
jadersonmarc <jadersonmarc@gmail.com>
2021-03-21 01:09:38 -0300

    implements authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,21 @@ class User < ApplicationRecord
             confirm_password
             password
             email
+            auth_token
         ]
 
          validates_presence_of :email
          validates_uniqueness_of :email
          validates_confirmation_of :password
+         validates_uniqueness_of :auth_token
+
+         def info
+          "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+        end
+
+        def generate_authentication_token!
+          begin
+            self.auth_token = Devise.friendly_token
+          end while User.exists?(auth_token: auth_token)
+        end
 end
