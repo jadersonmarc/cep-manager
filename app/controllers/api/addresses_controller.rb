73a181cee077d000e0097d981e8f1242ef08aad2
jadersonmarc <jadersonmarc@gmail.com>
2021-03-22 00:26:42 -0300

    add contrller addresses

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -1,14 +1,46 @@
 module Api
     class AddressesController < ApplicationController
-        def cep
-            '25220680'
-        end
-        
-        def index
-            account_id = '25220680'
-            endereco = RestService.new.entities(cep)
-            render json: endereco
-        end
+        respond_to :json
 
+        def index
+            
+            address = Address.joins("INNER JOIN users ON users.id = addresses.user_id ")
+            render json: { address: address }, status: 200
+        end
+    
+      def show
+        address = current_user.address.find(address_params[:id])
+        render json: address, status: 200
+      end
+    
+      def create
+        address_cep = address_params[:cep]
+        address = Address.new
+
+        response = RestService.new.send(address_cep)
+        user_address = JSON.parse(response.to_json)  
+
+        address.cep               = user_address['cep']
+        address.uf                = user_address['uf']
+        address.cidade            = user_address['cidade']
+        address.bairro            = user_address['bairro']
+        address.endereco_completo = user_address['logradouro']
+        address.user_id           = current_user.id
+
+        if address.save
+          render json: address, status: 201
+        else
+          render json: { errors: address.errors }, status: 422
+        end
+      end
+    
+
+
+        protected
+   
+        def address_params
+         params.required(:addresses)
+               .permit(:cep, :uf, :cidade, :bairro, :endereco_completo)
+        end
     end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,15 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
+
+  ATTRIBUTES = %i[
+    cep
+    uf
+    cidade
+    bairro
+    endereco_completo
+    user_id
+    email
+  ]
 
   validates_presence_of :cep
   validates_presence_of :user_id 
